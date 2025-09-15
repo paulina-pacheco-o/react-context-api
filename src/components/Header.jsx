37f91d5b2007ebb0616db4452b20d85d530be975
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-15 20:55:51 +0200

    button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,20 @@ const Header = () => {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <ul className="list-unstyled d-flex">
-            <li className="me-3">
-              <NavLink to="/">Homepage</NavLink>
-            </li>
-            <li className="me-3">
-              <NavLink to="/aboutus">Chi siamo</NavLink>
-            </li>
-            <li className="me-3">
-              <NavLink to="/productspage">Pagina prodotti</NavLink>
-            </li>
-          </ul>
+          <div className="d-flex justify-content-between mt-3">
+            <ul className="list-unstyled d-flex">
+              <li className="me-3">
+                <NavLink to="/">Homepage</NavLink>
+              </li>
+              <li className="me-3">
+                <NavLink to="/aboutus">Chi siamo</NavLink>
+              </li>
+              <li className="me-3">
+                <NavLink to="/productspage">Pagina prodotti</NavLink>
+              </li>
+            </ul>
+            <div className="btn btn-primary">Modalità Budget</div>
+          </div>
         </div>
       </div>
     </div>
